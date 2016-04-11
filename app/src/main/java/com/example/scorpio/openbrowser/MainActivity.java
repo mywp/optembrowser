60bfb15efe5faf6604ac96eb17f4e207483118a8
Scorpio <Scorpio@MacBook-Pro.workgroup>
2016-04-11 19:11:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/scorpio/openbrowser/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.scorpio.openbrowser;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void click(View view) {
+//        <action android:name="android.intent.action.VIEW" />
+//        <category android:name="android.intent.category.DEFAULT" />
+//        <category android:name="android.intent.category.BROWSABLE" />
+//        <data android:scheme="http" />
+//        <data android:scheme="https" />
+//        <data android:scheme="about" />
+//        <data android:scheme="javascript" />
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addCategory("android.intent.category.BROWSABLE");
+        intent.setData(Uri.parse("http://www.bing.com"));
+        startActivity(intent);
+    }
+}
